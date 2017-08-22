f2cc63e245fc7a2874749111c84bdcae6401ccba
yhding <ilooyo2014fz>
2017-08-22 10:54:45 +0800

    提交脚本修改.

--- openh264_32.bat
+++ openh264_32.bat
@@ -10,14 +10,19 @@ echo "make::"
 make 'OS=msvc' clean
 PATH=%PATH%;"../../bin/nasm32/";
 set PREFIX=../../bin/openh264_32
+
+dos2unix ./codec/common/*.sh
+
 make 'OS=msvc' 'ARCH=i686' 'PREFIX=%PREFIX%'
 echo "make install::"
-mkdir %PREFIX%
+mkdir "%PREFIX%"
 make 'OS=msvc' 'ARCH=i686' 'PREFIX=%PREFIX%' install
 
-cp -f *.lib %PREFIX%/lib
-cp -f *.pdb %PREFIX%/lib
-cp -f *.exe %PREFIX%/bin
-cp -f *.dll %PREFIX%/bin
+mkdir " %PREFIX%/lib/"
+mkdir "%PREFIX%/bin/"
+cp -f *.lib %PREFIX%/lib/
+cp -f *.pdb %PREFIX%/lib/
+cp -f *.exe %PREFIX%/bin/
+cp -f *.dll %PREFIX%/bin/
 
 cd %ROOT%
--- openh264_64.bat
+++ openh264_64.bat
@@ -9,15 +9,20 @@ echo "make::"
 make 'OS=msvc' clean
 set PATH=%PATH%;"%ROOT%/bin/nasm64/";
 set PREFIX=../../bin/openh264_64
+
+dos2unix ./codec/common/*.sh
+
 make -j4 'OS=msvc' 'ARCH=x86_64' 'ENABLE64BIT=Yes' 'USE_ASM=Yes' 'PREFIX=%PREFIX%'
 
 echo "make install::"
-mkdir %PREFIX%
-make 'OS=msvc' 'ARCH=x86_64' 'ENABLE64BIT=Yes' 'USE_ASM=Yes' 'PREFIX=%PREFIX%' install 
+mkdir "%PREFIX%"
+make 'OS=msvc' 'ARCH=x86_64' 'ENABLE64BIT=Yes' 'USE_ASM=Yes' 'PREFIX=%PREFIX%' install
 
-cp -f *.lib %PREFIX%/lib
-cp -f *.pdb %PREFIX%/lib
-cp -f *.exe %PREFIX%/bin
-cp -f *.dll %PREFIX%/bin
+mkdir " %PREFIX%/lib/"
+mkdir "%PREFIX%/bin/"
+cp -f *.lib %PREFIX%/lib/
+cp -f *.pdb %PREFIX%/lib/
+cp -f *.exe %PREFIX%/bin/
+cp -f *.dll %PREFIX%/bin/
 
 cd %ROOT%
